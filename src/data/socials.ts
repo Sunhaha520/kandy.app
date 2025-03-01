4eedccebe7cf9af93bd72f6f9922b20b5ff064ff
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2025-03-02 03:26:19 +0800

    ggg

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,27 +1,27 @@
 import IconTwitter from "../icons/twitter.astro";
 import IconGithub from "../icons/github.astro";
-import IconLinkedin from "../icons/linkedin.astro";
-import IconDiscord from "../icons/discord.astro";
+import IconBlog from "../icons/blog.astro";
+import IconDiscord from "../icons/qq.astro";
 
 export const SOCIALS = [
   {
     name: "Twitter",
-    url: "https://x.com/AndrewKuktenko",
+    url: "https://x.com/DerekSunZY",
     icon: IconTwitter,
   },
   {
     name: "Github",
-    url: "https://github.com/AndrewKuktenko",
+    url: "https://github.com/Sunhaha520",
     icon: IconGithub,
   },
   {
-    name: "Linkedin",
-    url: "https://www.linkedin.com/in/andrew-kuktenko/",
-    icon: IconLinkedin,
+    name: "Blog",
+    url: "https://blog.kelejun.cn",
+    icon: IconBlog,
   },
   {
-    name: "Discord",
-    url: "https://discord.com/users/342345918533599236",
+    name: "QQ",
+    url: "tencent://message/?uin=2567689488&Site=&Menu=yes",
     icon: IconDiscord,
   },
 ];
